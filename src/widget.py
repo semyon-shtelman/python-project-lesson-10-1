82ea56dc8468a941c643b9c312a86978ab6b0972
Semen Shtelman <shtelmaan@gmail.com>
2025-08-28 15:17:37 +0600

    modification of the module

--- src/widget.py
+++ src/widget.py
@@ -1,14 +1,16 @@
-from masks import get_mask_card_number, get_mask_account
+from masks import get_mask_account, get_mask_card_number
 
 
-def mask_account_card(account_card: str) -> str:
+def mask_account_card(account_card: str) -> str | None:
     """Возвращает строку с замаскированным номером.
-     Для карт и счетов используется разные типы маскировки"""
+    Для карт и счетов используется разные типы маскировки"""
     account_card_number = "".join([i for i in account_card if i.isdigit()])
-    if len(account_card_number) <= 16:
-        return f"{account_card[:-16]}{get_mask_card_number(int(account_card_number))}"
-    else:
-        return f"{account_card[:-20]}{get_mask_account(int(account_card_number))}"
+    account_card_name = "".join([i for i in account_card if not i.isdigit()])
+    if len(account_card_number) == 16:
+        return f"{account_card_name} {get_mask_card_number(int(account_card_number))}"
+    elif len(account_card_number) == 20:
+        return f"{account_card_name} {get_mask_account(int(account_card_number))}"
+    return None
 
 
 def get_date(date: str) -> str:
