982742b0c446b84ce25bd40268ccfa22c46023f4
Semyon Shtelman <shtelmaan@gmail.com>
2025-09-08 21:48:23 +0600

    create a new module

---
+++ src/processing.py
@@ -0,0 +1,14 @@
+def filter_by_state(get_list_dict: list[dict],
+                    state: str = "EXECUTED") -> list[dict]:
+    """Возвращает отфильтрованный список словарей,
+    где state соответствует заданному значению
+    """
+    pass
+
+def sort_by_date(get_list_dict: list[dict],
+                 reverse: bool = True) -> list[dict]:
+    """принимает список словарей и необязательный параметр,
+    задающий порядок сортировки (по умолчанию — убывание).
+    Функция возвращает новый список словарей,
+    отсортированный по дате (date) """
+    pass
