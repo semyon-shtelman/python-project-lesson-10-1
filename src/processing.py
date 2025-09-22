53108c243bdbbf93f22bdce529b69e0be3cf437e
Semyon Shtelman <shtelmaan@gmail.com>
2025-09-22 23:43:27 +0600

    refactor: optimize processing functions and add tests

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,7 @@
-from typing import Any, Dict, List
+from typing import Any
 
 
-def filter_by_state(transactions: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
+def filter_by_state(transactions: list[dict[str, Any]], state: str = "EXECUTED") -> list[dict[str, Any]]:
     """
     Фильтрует список транзакций по статусу.
 
@@ -9,12 +9,17 @@ def filter_by_state(transactions: List[Dict[str, Any]], state: str = "EXECUTED")
     :param state: Статус транзакций для фильтрации. По умолчанию "EXECUTED"
 
     :return: Новый список словарей, содержащий только транзакции с указанным статусом.
-    Если подходящих транзакций нет, возвращается пустой список.
+    :raises ValueError: Если подходящих транзакций не найдено
+                        или входной список пуст.
     """
-    return [item for item in transactions if item.get("state") == state]
+    transaction = [item for item in transactions if item.get("state") == state]
+    if transaction:
+        return transaction
+    else:
+        raise ValueError("Подходящих транзакций не найдено " "или входной список пуст.")
 
 
-def sort_by_date(transactions: List[Dict[str, Any]], reverse: bool = True) -> List[Dict[str, Any]]:
+def sort_by_date(transactions: list[dict[str, Any]], reverse: bool = True) -> list[dict[str, Any]]:
     """
     Сортирует список транзакций по дате.
 
@@ -22,7 +27,7 @@ def sort_by_date(transactions: List[Dict[str, Any]], reverse: bool = True) -> Li
     :param reverse: Если True(по умолчанию) - сортировка от новых к старым,
     если False - от старых к новым
 
-    :return: Отсортированный список транзакций по дате. Если подходящих транзакций нет,
-    KeyError: Если в какой-либо транзакции отсутствует ключ 'date'.
+    :return: Новый список транзакций, отсортированных по дате.
+    :raises KeyError: Если в какой-либо транзакции отсутствует ключ 'date'.
     """
     return sorted(transactions, key=lambda x: x["date"], reverse=reverse)
