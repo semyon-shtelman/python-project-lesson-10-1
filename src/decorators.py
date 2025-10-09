aa41af2208c85321cfabb93484cf806128f4efed
Semyon Shtelman <shtelmaan@gmail.com>
2025-10-10 04:08:42 +0600

    add docstring

--- src/decorators.py
+++ src/decorators.py
@@ -2,11 +2,23 @@ from functools import wraps
 
 
 def log(filename=None):
+    """
+    Декоратор log для логирования выполнения функции.
+
+    При успешном выполнении функции:
+        - Выводит в консоль или пишет в файл строку "<имя_функции> ok"
+
+    При возникновении ошибки:
+        - Выводит в консоль или пишет в файл строку
+          "<имя_функции> error: <тип_ошибки>. Inputs: <args>, <kwargs>"
+        - Пробрасывает оригинальное исключение дальше
+    """
+
     def decorator(function):
         @wraps(function)
         def wrapper(*args, **kwargs):
             try:
-                result = function(*args)
+                result = function(*args, **kwargs)
                 message = f"{function.__name__} ok"
                 if filename:
                     with open(filename, "a", encoding="UTF-8") as f:
